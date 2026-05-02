4a648555714a16c9d01a2531d45e51d38170ce74
Kolibra <kkoliba@gmail.com>
2026-05-02 06:15:04 -0700

    Add Todo Functionality

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+// let deleteID = 0;
+
 document.querySelector(".add-button").addEventListener("click", function () {
   const todoContent = document.querySelector(".todo").value;
   const todoList = document.querySelector(".todo-list");
   const todoEl = document.createElement("p");
   todoList.appendChild(todoEl);
-  todoEl.innerHTML = `${todoContent}`;
+  todoEl.innerHTML = `${todoContent} <button>DELETE</button>`;
+
+  /*const delButton = document.querySelector(`#delete-button-${deleteID}`);
+  delButton.addEventListener("click", function () {
+    const delParentPara = delButton.parentNode;
+    delParentPara.parentNode.removeChild(delParentPara);
+  });
+  deleteID++; */
 });
