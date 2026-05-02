20dd097c07740499dc03893156e042ea0c58ac60
Kolibra <kkoliba@gmail.com>
2026-05-02 06:43:11 -0700

    Extending the Todo Functionality

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ let deleteID = 0;
 
 document.querySelector(".add-button").addEventListener("click", function () {
   const todoContent = document.querySelector(".todo").value;
+  const dateContent = document.querySelector(".date").value;
   const todoList = document.querySelector(".todo-list");
   const todoEl = document.createElement("p");
   todoList.appendChild(todoEl);
-  todoEl.innerHTML = `${todoContent} <button id ='delete-button-${deleteID}'>DELETE</button>`;
+  todoEl.innerHTML = `${todoContent}  ${dateContent} <button id ='delete-button-${deleteID}'>DELETE</button>`;
 
   const delButton = document.querySelector(`#delete-button-${deleteID}`);
   delButton.addEventListener("click", function () {
